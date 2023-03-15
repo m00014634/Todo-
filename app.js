bac0c378bab2fae0416ef322a6df55142ce660bc
John Doe <johndoe@gmail.com>
2023-03-15 16:01:27 +0500

    init project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.set('view engine', 'pug')
+
+// http://localhost:8000
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+
+
+app.listen(PORT, (err)=>{
+    if (err) throw err
+
+    console.log(`This app is running on PORT' ${ PORT }`)
+})
